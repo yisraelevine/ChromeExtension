613e6e1093b4f35d0839c969259b9302bf387cdb
yisraelevine <yisraelevine@gmail.com>
2024-03-03 03:27:32 -0500

    Improved Fullscreen Handling with Wake Lock Implementation and Font Size Adjustment

--- content.js
+++ content.js
@@ -1,4 +1,4 @@
-// Selectors for different study elements
+// Study element selectors
 const selectors = [
     '.content > [dir="rtl"]',      // Chumash, Tanya, Sefer Hamitzvos
     '.daily_study_body',           // Tehillim
@@ -6,19 +6,37 @@ const selectors = [
     '.content > .hebrew'           // Rambam 1 & 3
 ]
 
-// Combine selectors into a single string
+// Combine selectors into single string
 const selector = selectors.join(', ')
 
-// Find the first matching study element
+// Find first study element
 const studyElement = document.querySelector(selector)
 
-// If a study element is found, apply fullscreen functionality
+// Apply fullscreen functionality if study element is present
 if (studyElement) {
     // Add class for styling
     studyElement.classList.add('study-element')
 
-    // Add event listener to enter fullscreen mode on click
-    studyElement.addEventListener('click', () => {
-        document.documentElement.requestFullscreen()
+    // Listen for click on studyElement to enter fullscreen
+    studyElement.addEventListener('click', async () => {
+        // Fullscreen request if not active
+        if (!document.fullscreenElement) {
+            document.documentElement.requestFullscreen()
+        }
+    })
+
+    // Store wake lock instance
+    let wakeLock
+
+    // Listen for changes in fullscreen mode
+    document.addEventListener('fullscreenchange', async () => {
+        // Enable wake lock to prevent screen from sleeping in fullscreen mode
+        if (document.fullscreenElement) {
+            wakeLock = await navigator.wakeLock.request('screen')
+        }
+        // Release wake lock upon exiting fullscreen
+        else if (wakeLock) {
+            await wakeLock.release()
+        }
     })
 }
